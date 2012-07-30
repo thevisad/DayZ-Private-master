5b3d5219be788c1c734a2219211e5e6df271405d
ayan4m1 <andrew@bulletlogic.com>
2012-07-30 09:54:36 -0400

    closed #13 (added build script, thanks to kegetys for cpbo!)

---
+++ sanctuary/repack.bat
@@ -0,0 +1,3 @@
+@echo off
+mkdir @Sanctuary\addons
+cpbo.exe -y -p dayz_server @Sanctuary\addons\dayz_server.pbo
---
+++ sanctuary/repack.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+mkdir -p @Sanctuary/addons
+wine cpbo -y -p dayz_server @Sanctuary/addons/dayz_server.pbo
