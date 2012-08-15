0b31e600d47f3300054379e92d7a221ef7c58f8c
ayan4m1 <andrew@bulletlogic.com>
2012-08-15 07:03:42 -0400

    add remoteexec.txt and createvehicle.txt to update_scripts.bat/sh

--- update_scripts.bat
+++ update_scripts.bat
@@ -1,3 +1,5 @@
 @echo off
 cd deploy\Sanctuary\BattlEye
 ..\..\..\util\wget.exe -N http://dl.dropbox.com/u/18463425/DayZserver/scripts.txt
+..\..\..\util\wget.exe -N http://dl.dropbox.com/u/18463425/DayZserver/remoteexec.txt
+..\..\..\util\wget.exe -N http://dl.dropbox.com/u/18463425/DayZserver/createvehicle.txt
--- update_scripts.sh
+++ update_scripts.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 cd deploy/Sanctuary/BattlEye/
 wget -N http://dl.dropbox.com/u/18463425/DayZserver/scripts.txt
+wget -N http://dl.dropbox.com/u/18463425/DayZserver/remoteexec.txt
+wget -N http://dl.dropbox.com/u/18463425/DayZserver/createvehicle.txt
