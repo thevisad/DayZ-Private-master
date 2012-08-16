922c9486e89b416b3b40403ff6e83cb77547e164
ayan4m1 <andrew@bulletlogic.com>
2012-08-16 07:15:16 -0400

    closes #3 (clean up argument parsing and debug messages in vehicles.pl)

--- vehicles.pl
+++ vehicles.pl
@@ -1,28 +1,40 @@
 #!/usr/bin/perl -w
 # Author: Guru Abdul
 # Script generating vehicles for DayZ Arma mod
-use v5.14;
+
 use POSIX;
 use DBI;
 use DBD::mysql;
-print "Vehicle generating script started!\n";
-my $dbinstance=0;
-my $dbuser = "dayz";
-my $db = "dayz";
-my $pw;
-my $globalLimit=100;
-given (scalar(@ARGV))
-{
-	when(5) {$dbinstance = $ARGV[0];$db = $ARGV[1];$dbuser= $ARGV[2];$pw = $ARGV[3];$globalLimit=$ARGV[4];}
-	when(4) {$dbinstance = $ARGV[0];$db = $ARGV[1];$dbuser= $ARGV[2];$pw = $ARGV[3];}
-	when(3) {$dbinstance = $ARGV[0];$dbuser= $ARGV[1];$pw = $ARGV[2];}
-	when(2) {$dbinstance = $ARGV[0];$pw = $ARGV[1];}
-	default {die "You can provide up to 5 arguments each separated by spaces in the following order:\n\t2 args: instance passToDB\n\t3 args: instance userDB passToDB\n\t4 args: instance database userDB passToDB\n;\t5 args: instance database userDB passToDB maxNumberOfVehicles\n"
-			}
-}
-my $dsn = "dbi:mysql:$db:localhost:3306";
-print "Generating vehicles for instance: ".$dbinstance." , with user: ".$dbuser."\n";
-my $dbh = DBI->connect($dsn, $dbuser, $pw) or die "Couldn't connect to db: ".DBI->errstr;
+use Getopt::Long;
+
+print "INFO: Started vehicle insertion.\n";
+
+my %args = ();
+
+GetOptions(
+	\%args,
+	'instance|index|i=s',
+	'hostname|host|dbhost|h=s',
+	'username|user|dbuser|u=s',
+	'password|pass|dbpass|p=s',
+	'database|dbname|d=s',
+	'port|dbport=s',
+	'help'
+);
+
+my %db = (
+	'host' => $args{'hostname'} ? $args{'hostname'} : 'localhost',
+	'instance' => $args{'instance'} ? $args{'instance'} : '1',
+	'limit' => $args{'limit'} ? $args{'limit'} : '500',
+	'user' => $args{'username'} ? $args{'username'} : 'dayz',
+	'pass' => $args{'password'} ? $args{'password'} : 'dayz',
+	'name' => $args{'database'} ? $args{'database'} : 'dayz',
+	'port' => $args{'port'} ? $args{'port'} : '3306'
+);
+
+my $dsn = "dbi:mysql:$db{'name'}:$db{'host'}:$db{'port'}";
+print "Generating vehicles for instance: ".$db{'instance'}." , with user: ".$db{'user'}."\n";
+my $dbh = DBI->connect($dsn, $db{'user'}, $db{'pass'}) or die "Couldn't connect to db: ".DBI->errstr;
 my $sth = $dbh->prepare('DELETE FROM objects WHERE damage>=0.95 OR (lastupdate<DATE_SUB(NOW(),INTERVAL 5 DAY) AND NOT otype="TentStorage");') or die;
 $sth->execute() or die "Couldn't execute statement" . $sth->errstr;#Clean dead vehicles
 my $numGenerated=0;#counter for the number of generated vehicles
@@ -32,7 +44,7 @@ my @chances = (0.65,0.7,0.65,0.7,0.95,0.25,0.55,0.55,0.75,0.55,0.55,0.55,0.55,0.
 my $n=0;
 my $do=0;
 $sth = $dbh->prepare("SELECT COUNT(*) FROM objects WHERE instance=? AND otype NOT IN ('TentStorage','Hedgehog_DZ','Wire_cat1')") or die;
-$sth->execute($dbinstance) or die;
+$sth->execute($db{'instance'}) or die;
 my $globalVehicleCount;
 my @d =$sth->fetchrow_array();
 $globalVehicleCount= $d[0];
@@ -42,27 +54,26 @@ my $query = "INSERT INTO objects (uid,pos,health,damage,otype,instance) VALUES "
 for (my $i=0;$i<scalar @vehicles;$i++)
 {
 	my $vehicle = $vehicles[$i];
-	$sth->execute($vehicle,$dbinstance) or die;
+	$sth->execute($vehicle,$db{'instance'}) or die;
 	my @data = $sth->fetchrow_array();
 	my $vehicleCount = $data[0];
 	my $spawnCount = 0;
 	my $chance = $chances[$i];
 	my $chanceFactor=1.5;
 	my $random = rand();
-	print "Chance: ".$chance." Random: ".$random."\n";
+	#print "Chance: ".$chance." Random: ".$random."\n";
 	my $limit = $vehicleLimits[$i]-$vehicleCount;
 	while($chance>$random && $spawnCount<$limit)
 	{
 		$chance /= $chanceFactor;
 		$chanceFactor += 0.14;
 		$spawnCount++;
-		print "ModChance: ".$chance."\n";
+		#print "ModChance: ".$chance."\n";
 	}
-	print "Generating ".$spawnCount." vehicles of type: ".$vehicle."\n";
-	print "Fetching random spawn points\n";
+	print "INFO: Generating ".$spawnCount." vehicles of type: ".$vehicle."\n";
 	my $sts = $dbh->prepare('SELECT * FROM spawns WHERE otype like ? AND NOT uuid IN (SELECT uid FROM objects WHERE instance = ?) ORDER BY RAND() LIMIT ?') or die;
-	$sts->execute($vehicle,$dbinstance,$spawnCount) or die;
-	while ((@data = $sts->fetchrow_array())&&$globalVehicleCount+$n<$globalLimit)
+	$sts->execute($vehicle,$db{'instance'},$spawnCount) or die;
+	while ((@data = $sts->fetchrow_array())&&$globalVehicleCount+$n<$db{'limit'})
 	{
 		print "Generating vehicle parts damage!\n";
 		my $health="";
@@ -88,17 +99,17 @@ for (my $i=0;$i<scalar @vehicles;$i++)
 			@parts = ('["palivo",1]','["motor",1]','["karoserie",1]','["wheel_1_1_steering",1]','["wheel_1_2_steering",1]','["wheel_2_1_steering",1]','["wheel_2_2_steering",1]');
 			$health = genDamage(@parts);
 		}
-		print "\t".$health."\n";
+		print "INFO: Damaged parts are ".$health."\n";
 		#add
 		#uid,pos,health,damage,otype,instance
 		if($first==0)
 		{
 			$first=1;
-			$query.="($data[3],'$data[1]','[$health]',$damage,'$data[2]',$dbinstance)";
+			$query.="($data[3],'$data[1]','[$health]',$damage,'$data[2]',$db{'instance'})";
 		}
 		else
 		{
-			$query.=",($data[3],'$data[1]','[$health]',$damage,'$data[2]',$dbinstance)";
+			$query.=",($data[3],'$data[1]','[$health]',$damage,'$data[2]',$db{'instance'})";
 		}
 		$do=1;
 		$n++;
@@ -111,11 +122,11 @@ if($do==1)
 {
 	print $query."\n";
 	$sth->execute() or die "Insert query failed";
-	print "ALL IS DONE! Spawed $n randomly damaged vehicles!";
+	print "INFO: Spawed $n randomly damaged vehicles!";
 }
 else
 {
-	print "Reached maximum vehicle limit for these types of vehicles! Not spawning any vehicles...";
+	print "ERROR: Reached maximum vehicle limit for these types of vehicles! Not spawning any vehicles...";
 }
 sub genDamage
 {
@@ -128,13 +139,13 @@ sub genDamage
 	my $chanceFactor = 1.15;
 	my @parts = @_;
 	my @restricted;
-	print "Chance: ".$chance." Random: ".$random."\n";
+	#print "Chance: ".$chance." Random: ".$random."\n";
 	while($chance>$random && $damParts<scalar @parts)
 	{
 		$chance /= $chanceFactor;
 		$chanceFactor += 0.15;
 		$damParts++;
-		print "ModChance: ".$chance."\n";
+		#print "ModChance: ".$chance."\n";
 	}
 	$damCount=0;
 	while($damParts>$damCount)
