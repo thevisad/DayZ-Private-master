f24365561b68712a5e18974f749275d465f638d4
ayan4m1 <andrew@bulletlogic.com>
2012-08-16 19:55:06 -0400

    add @BlissLingor to repack scripts

--- repack.bat
+++ repack.bat
@@ -1,6 +1,8 @@
 @echo off
 mkdir deploy\@Bliss\addons
+mkdir deploy\@BlissLingor\addons
 mkdir deploy\MPMissions
 util\cpbo.exe -y -p bliss\dayz_server deploy\@Bliss\addons\dayz_server.pbo
+util\cpbo.exe -y -p bliss\dayz_lingor deploy\@BlissLingor\addons\dayz_server.pbo
 util\cpbo.exe -y -p bliss\missions\dayz.lingor deploy\MPMissions\dayz.lingor.pbo
 util\cpbo.exe -y -p bliss\missions\dayz.chernarus deploy\MPMissions\dayz.chernarus.pbo
