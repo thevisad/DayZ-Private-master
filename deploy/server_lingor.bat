ca9899a3907aaf22f7879be7e4c158559e18cdeb
ayan4m1 <andrew@bulletlogic.com>
2012-08-23 19:48:17 -0400

    closes #60 (added scripts and profile directory for Lingor)

---
+++ deploy/server_lingor.bat
@@ -0,0 +1,1 @@
+"Expansion\beta\arma2oaserver.exe" -port=2302 "-config=BlissLingor\config.cfg" "-cfg=BlissLingor\basic.cfg" "-profiles=BlissLingor" -name=BlissLingor "-mod=@CBA;@CBA_A2;@CBA_OA;@Arma2NET;@DayZ;@BlissLingor;" -cpuCount=2 -maxMem=1578
