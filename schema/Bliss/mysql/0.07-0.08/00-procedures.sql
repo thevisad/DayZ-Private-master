7f1367702e6a5a6241a6eea4f33f35b5bab4db35
Rory King <rory_king@hotmail.co.uk>
2012-08-30 17:21:27 +0100

    Added comment markers and reverted INSERT IGNORE
    Fixed the lack of comment markers within the migration script
    Removed the IGNORE from INSERT IGNORE statements, a quick benchmark
    shows that this is roughly 4.8% faster.

--- schema/Bliss/mysql/0.07-0.08/00-procedures.sql
+++ schema/Bliss/mysql/0.07-0.08/00-procedures.sql
@@ -1,29 +1,29 @@
 DROP PROCEDURE IF EXISTS `delO`;
-CREATE PROCEDURE `proc_deleteObject`(IN `param_UniqueId` VARCHAR(50))
+CREATE PROCEDURE `proc_deleteObject`(IN `p_UniqueId` VARCHAR(50))
 	LANGUAGE SQL
 	NOT DETERMINISTIC
 	CONTAINS SQL
 	SQL SECURITY DEFINER
 	COMMENT ''
 BEGIN
-      DELETE FROM objects WHERE uid=param_UniqueId;
+      DELETE FROM objects WHERE uid=p_UniqueId; --
 END;
 
 
 DROP PROCEDURE IF EXISTS `getLoadout`;
-CREATE PROCEDURE `proc_getInstanceLoadout`(IN `param_InstanceId` INT)
+CREATE PROCEDURE `proc_getInstanceLoadout`(IN `p_InstanceId` INT)
 	LANGUAGE SQL
 	NOT DETERMINISTIC
 	CONTAINS SQL
 	SQL SECURITY DEFINER
 	COMMENT ''
 BEGIN
-   SELECT loadout FROM instances WHERE instance = param_InstanceId;
+   SELECT loadout FROM instances WHERE instance = p_InstanceId; --
 END;
 
 
 DROP PROCEDURE IF EXISTS `getO`;
-CREATE PROCEDURE `proc_getObjects`(IN `param_InstanceId` int, IN `param_CurrentPage` int)
+CREATE PROCEDURE `proc_getObjects`(IN `p_InstanceId` int, IN `p_CurrentPage` int)
 	LANGUAGE SQL
 	NOT DETERMINISTIC
 	CONTAINS SQL
@@ -31,13 +31,13 @@ CREATE PROCEDURE `proc_getObjects`(IN `param_InstanceId` int, IN `param_CurrentP
 	COMMENT ''
 BEGIN
 	DECLARE itemsPerPage INT DEFAULT 5; -- Must match proc_getObjectPageCount
-	DECLARE currentOffset INT DEFAULT (itemsPerPage * param_CurrentPage);
-	SELECT id, otype, oid, pos, inventory, health, fuel, damage FROM objects WHERE instance = param_InstanceId LIMIT currentOffset, itemsPerPage;
+	DECLARE currentOffset INT DEFAULT (itemsPerPage * p_CurrentPage); --
+	SELECT id, otype, oid, pos, inventory, health, fuel, damage FROM objects WHERE instance = p_InstanceId LIMIT currentOffset, itemsPerPage; --
 END;
 
 
 DROP PROCEDURE IF EXISTS `getOC`;
-CREATE PROCEDURE `proc_getObjectPageCount`(IN `param_InstanceId` int)
+CREATE PROCEDURE `proc_getObjectPageCount`(IN `p_InstanceId` int)
 	LANGUAGE SQL
 	NOT DETERMINISTIC
 	CONTAINS SQL
@@ -45,12 +45,12 @@ CREATE PROCEDURE `proc_getObjectPageCount`(IN `param_InstanceId` int)
 	COMMENT ''
 BEGIN
 	DECLARE itemsPerPage INT DEFAULT 5; -- Must match proc_getObjects
-	SELECT FLOOR(COUNT(*) / itemsPerPage) from objects WHERE instance = param_InstanceId;
+	SELECT FLOOR(COUNT(*) / itemsPerPage) from objects WHERE instance = p_InstanceId; --
 END;
 
 
 DROP PROCEDURE IF EXISTS `getTasks`;
-CREATE PROCEDURE `proc_getSchedulerTasks`(IN `param_InstanceId` int, IN `param_CurrentPage` int)
+CREATE PROCEDURE `proc_getSchedulerTasks`(IN `p_InstanceId` int, IN `p_CurrentPage` int)
 	LANGUAGE SQL
 	NOT DETERMINISTIC
 	CONTAINS SQL
@@ -58,13 +58,13 @@ CREATE PROCEDURE `proc_getSchedulerTasks`(IN `param_InstanceId` int, IN `param_C
 	COMMENT ''
 BEGIN
 	DECLARE itemsPerPage INT DEFAULT 10; -- Must match proc_getSchedulerTaskPageCount
-	DECLARE currentOffset INT DEFAULT (itemsPerPage * param_CurrentPage);
-	SELECT message, mtype, looptime, mstart FROM scheduler JOIN instances ON instances.mvisibility = scheduler.visibility WHERE instances.instance = param_InstanceId LIMIT currentOffset, itemsPerPage;
+	DECLARE currentOffset INT DEFAULT (itemsPerPage * p_CurrentPage); --
+	SELECT message, mtype, looptime, mstart FROM scheduler JOIN instances ON instances.mvisibility = scheduler.visibility WHERE instances.instance = p_InstanceId LIMIT currentOffset, itemsPerPage; --
 END;
 
 
 DROP PROCEDURE IF EXISTS `getTC`;
-CREATE PROCEDURE `proc_getSchedulerTaskPageCount`(IN `param_InstanceId` int)
+CREATE PROCEDURE `proc_getSchedulerTaskPageCount`(IN `p_InstanceId` int)
 	LANGUAGE SQL
 	NOT DETERMINISTIC
 	CONTAINS SQL
@@ -72,11 +72,11 @@ CREATE PROCEDURE `proc_getSchedulerTaskPageCount`(IN `param_InstanceId` int)
 	COMMENT ''
 BEGIN
 	DECLARE itemsPerPage INT DEFAULT 10; -- Must match proc_getSchedulerTasks
-	SELECT FLOOR(COUNT(*) / itemsPerPage) FROM scheduler JOIN instances ON instances.mvisibility = scheduler.visibility WHERE instances.instance = param_InstanceId;
+	SELECT FLOOR(COUNT(*) / itemsPerPage) FROM scheduler JOIN instances ON instances.mvisibility = scheduler.visibility WHERE instances.instance = p_InstanceId; --
 END;
 
 DROP PROCEDURE IF EXISTS `getTime`;
-CREATE PROCEDURE `proc_getInstanceTime`(IN `param_InstanceID` INT)
+CREATE PROCEDURE `proc_getInstanceTime`(IN `p_InstanceID` INT)
 	LANGUAGE SQL
 	NOT DETERMINISTIC
 	CONTAINS SQL
@@ -84,97 +84,97 @@ CREATE PROCEDURE `proc_getInstanceTime`(IN `param_InstanceID` INT)
 	COMMENT 'Gets the current game time for an instance, respecting the timezone offset as per defined in the instances table.'
 BEGIN
 	DECLARE server_time DATETIME DEFAULT NOW(); -- Declare a variable to hold the localised server time, the default is NOW() in case the instance param is invalid in the query below.
-	SELECT NOW() + INTERVAL (timezone) HOUR INTO server_time FROM instances WHERE instance = param_InstanceID; -- Select the current server DATETIME and modify it by the timezone offset, before stuffing it into the variable, skipped if timezone == null
+	SELECT NOW() + INTERVAL (timezone) HOUR INTO server_time FROM instances WHERE instance = p_InstanceID; -- Select the current server DATETIME and modify it by the timezone offset, before stuffing it into the variable, skipped if timezone == null
 	SELECT DATE_FORMAT(server_time,'%d-%m-%Y'), TIME_FORMAT(server_time, '%T'); -- Return the results.
 END;
 
 
 DROP PROCEDURE IF EXISTS `insOselI`;
-CREATE PROCEDURE `proc_insertObject`(IN `param_ObjectUniqueId` VARCHAR(50), IN `param_ObjectType` VARCHAR(255), IN `param_ObjectHealth` VARCHAR(1024), IN `param_ObjectDamage` DOUBLE, IN `param_ObjectFuel` DOUBLE, IN `param_ObjectOwner` INT, IN `param_ObjectPosition` VARCHAR(255), IN `param_InstanceId` INT)
+CREATE PROCEDURE `proc_insertObject`(IN `p_ObjectUniqueId` VARCHAR(50), IN `p_ObjectType` VARCHAR(255), IN `p_ObjectHealth` VARCHAR(1024), IN `p_ObjectDamage` DOUBLE, IN `p_ObjectFuel` DOUBLE, IN `p_ObjectOwner` INT, IN `p_ObjectPosition` VARCHAR(255), IN `p_InstanceId` INT)
 	LANGUAGE SQL
 	NOT DETERMINISTIC
 	CONTAINS SQL
 	SQL SECURITY DEFINER
 	COMMENT 'Inserts a new object into the Database'
 BEGIN
-	INSERT IGNORE INTO objects (uid,otype,health,damage,oid,pos,fuel,instance) VALUES (param_ObjectUniqueId, param_ObjectType, param_ObjectHealth, param_ObjectDamage, param_ObjectOwner, param_ObjectPosition, param_ObjectFuel, param_InstanceId);
+	INSERT INTO objects (uid,otype,health,damage,oid,pos,fuel,instance) VALUES (p_ObjectUniqueId, p_ObjectType, p_ObjectHealth, p_ObjectDamage, p_ObjectOwner, p_ObjectPosition, p_ObjectFuel, p_InstanceId); --
 END;
 
 DROP PROCEDURE IF EXISTS `insUNselI`;
-CREATE PROCEDURE `proc_insertPlayer`(IN `param_PlayerUniqueId` varchar(128), IN `param_PlayerName` varchar(255))
+CREATE PROCEDURE `proc_insertPlayer`(IN `p_PlayerUniqueId` varchar(128), IN `p_PlayerName` varchar(255))
 	LANGUAGE SQL
 	NOT DETERMINISTIC
 	CONTAINS SQL
 	SQL SECURITY DEFINER
 	COMMENT ''
 BEGIN
-	INSERT INTO main (uid, name, survival) VALUES (param_PlayerUniqueId, param_PlayerName, NOW());
-	SELECT LAST_INSERT_ID();
+	INSERT INTO main (uid, name, survival) VALUES (p_PlayerUniqueId, p_PlayerName, NOW()); --
+	SELECT LAST_INSERT_ID(); --
 END;
 
 DROP PROCEDURE IF EXISTS `logLogin`;
-CREATE PROCEDURE `proc_logLogin`(IN `param_PlayerUniqueId` varchar(50))
+CREATE PROCEDURE `proc_logLogin`(IN `p_PlayerUniqueId` varchar(50))
 	LANGUAGE SQL
 	NOT DETERMINISTIC
 	CONTAINS SQL
 	SQL SECURITY DEFINER
 	COMMENT ''
 BEGIN
-	INSERT IGNORE INTO log_entry (profile_id, log_code_id) VALUES (param_PlayerUniqueId, 1);
+	INSERT INTO log_entry (profile_id, log_code_id) VALUES (p_PlayerUniqueId, 1); --
 END;
 
 
 DROP PROCEDURE IF EXISTS `logLogout`;
-CREATE PROCEDURE `proc_logLogout`(IN `param_PlayerUniqueId` varchar(50))
+CREATE PROCEDURE `proc_logLogout`(IN `p_PlayerUniqueId` varchar(50))
 	LANGUAGE SQL
 	NOT DETERMINISTIC
 	CONTAINS SQL
 	SQL SECURITY DEFINER
 	COMMENT ''
 BEGIN
-	INSERT IGNORE INTO log_entry (profile_id, log_code_id) VALUES (param_PlayerUniqueId, 2);
+	INSERT INTO log_entry (profile_id, log_code_id) VALUES (p_PlayerUniqueId, 2); --
 END;
 
 
 DROP PROCEDURE IF EXISTS `selIIBSM`;
-CREATE PROCEDURE `proc_getPlayer`(IN `param_PlayerUniqueId` varchar(128), IN `param_PlayerName` VARCHAR(128))
+CREATE PROCEDURE `proc_getPlayer`(IN `p_PlayerUniqueId` varchar(128), IN `p_PlayerName` VARCHAR(128))
 	LANGUAGE SQL
 	NOT DETERMINISTIC
 	CONTAINS SQL
 	SQL SECURITY DEFINER
 	COMMENT 'Select ID, Inventory, Backpack, Survival Time and Model.'
 BEGIN 
-	UPDATE main SET name = param_PlayerName WHERE uid = param_PlayerUniqueId;
-	SELECT id, inventory, backpack, FLOOR(TIME_TO_SEC(TIMEDIFF(NOW(), survival))/60), model, late, ldrank FROM main WHERE uid = param_PlayerUniqueId AND death = 0;
+	UPDATE main SET name = p_PlayerName WHERE uid = p_PlayerUniqueId; --
+	SELECT id, inventory, backpack, FLOOR(TIME_TO_SEC(TIMEDIFF(NOW(), survival))/60), model, late, ldrank FROM main WHERE uid = p_PlayerUniqueId AND death = 0; --
 END;
 
 
 
 DROP PROCEDURE IF EXISTS `selMPSSH`;
-CREATE PROCEDURE `proc_getPlayerStats`(IN `param_PlayerID` INT)
+CREATE PROCEDURE `proc_getPlayerStats`(IN `p_PlayerID` INT)
 	LANGUAGE SQL
 	NOT DETERMINISTIC
 	CONTAINS SQL
 	SQL SECURITY DEFINER
 	COMMENT ''
 BEGIN
-      SELECT medical, pos, kills, state, humanity, hs, hkills, bkills FROM main WHERE id = param_PlayerID AND death=0;
+      SELECT medical, pos, kills, state, humanity, hs, hkills, bkills FROM main WHERE id = p_PlayerID AND death=0; --
 END;
 
 
 DROP PROCEDURE IF EXISTS `setCD`;
-CREATE PROCEDURE `proc_setPlayerDead`(IN `param_PlayerId` INT)
+CREATE PROCEDURE `proc_setPlayerDead`(IN `p_PlayerId` INT)
 	LANGUAGE SQL
 	NOT DETERMINISTIC
 	CONTAINS SQL
 	SQL SECURITY DEFINER
 	COMMENT ''
 BEGIN
-      UPDATE main SET death=1 WHERE id = param_PlayerId;
+      UPDATE main SET death=1 WHERE id = p_PlayerId; --
 END;
 
 DROP PROCEDURE IF EXISTS `update`;
-CREATE PROCEDURE `proc_updatePlayer`(IN `param_PlayerId` int, IN `param_PlayerPositon` varchar(1024), IN `param_PlayerInventory` varchar(2048), IN `param_PlayerBackpack` varchar(2048), IN `param_PlayerMedicalStatus` varchar(1024), IN `param_PlayerLastAte` int, IN `param_PlayerLastDrank` int, IN `param_PlayerSurvivalTime` int, IN `param_PlayerModel` varchar(255), IN `param_PlayerHumanity` int, IN `param_PlayerZombieKills` int, IN `param_PlayerHeadshots` int, IN `param_PlayerMurders` int, IN `param_PlayerBanditKills` int, IN `param_PlayerState` varchar(255))
+CREATE PROCEDURE `proc_updatePlayer`(IN `p_PlayerId` int, IN `p_PlayerPositon` varchar(1024), IN `p_PlayerInventory` varchar(2048), IN `p_PlayerBackpack` varchar(2048), IN `p_PlayerMedicalStatus` varchar(1024), IN `p_PlayerLastAte` int, IN `p_PlayerLastDrank` int, IN `p_PlayerSurvivalTime` int, IN `p_PlayerModel` varchar(255), IN `p_PlayerHumanity` int, IN `p_PlayerZombieKills` int, IN `p_PlayerHeadshots` int, IN `p_PlayerMurders` int, IN `p_PlayerBanditKills` int, IN `p_PlayerState` varchar(255))
 	LANGUAGE SQL
 	NOT DETERMINISTIC
 	CONTAINS SQL
@@ -182,80 +182,80 @@ CREATE PROCEDURE `proc_updatePlayer`(IN `param_PlayerId` int, IN `param_PlayerPo
 	COMMENT ''
 BEGIN
 	UPDATE main SET
-		kills=kills+param_PlayerZombieKills,
-		hs=hs+param_PlayerHeadshots,
-		bkills=bkills+param_PlayerBanditKills,
-		hkills=hkills+param_PlayerMurders,
-		state=param_PlayerState,
-		model=IF(param_PlayerModel='any',model,param_PlayerModel),
-		late=IF(param_PlayerLastAte=-1,0,late+param_PlayerLastAte),
-		ldrank=IF(param_PlayerLastDrank<-1,0,ldrank+param_PlayerLastDrank),
-		stime=stime+param_PlayerSurvivalTime,
-		pos=IF(param_PlayerPositon='[]', pos, param_PlayerPositon),
-		humanity=IF(param_PlayerHumanity=0, humanity, param_PlayerHumanity),
-		medical=IF(param_PlayerMedicalStatus='[]', medical, param_PlayerMedicalStatus),
-		backpack=IF(param_PlayerBackpack='[]', backpack, param_PlayerBackpack),
-		inventory=IF(param_PlayerInventory='[]', inventory, param_PlayerInventory)
-	WHERE id = param_PlayerId;
+		kills=kills+p_PlayerZombieKills,
+		hs=hs+p_PlayerHeadshots,
+		bkills=bkills+p_PlayerBanditKills,
+		hkills=hkills+p_PlayerMurders,
+		state=p_PlayerState,
+		model=IF(p_PlayerModel='any',model,p_PlayerModel),
+		late=IF(p_PlayerLastAte=-1,0,late+p_PlayerLastAte),
+		ldrank=IF(p_PlayerLastDrank<-1,0,ldrank+p_PlayerLastDrank),
+		stime=stime+p_PlayerSurvivalTime,
+		pos=IF(p_PlayerPositon='[]', pos, p_PlayerPositon),
+		humanity=IF(p_PlayerHumanity=0, humanity, p_PlayerHumanity),
+		medical=IF(p_PlayerMedicalStatus='[]', medical, p_PlayerMedicalStatus),
+		backpack=IF(p_PlayerBackpack='[]', backpack, p_PlayerBackpack),
+		inventory=IF(p_PlayerInventory='[]', inventory, p_PlayerInventory)
+	WHERE id = p_PlayerId; --
 END;
 
 DROP PROCEDURE IF EXISTS `updIH`;
-CREATE PROCEDURE `proc_updateObjectHealth`(IN `param_ObjectId` INT, IN `param_ObjectHealth` VARCHAR(1024), IN `param_ObjectDamage` DOUBLE)
+CREATE PROCEDURE `proc_updateObjectHealth`(IN `p_ObjectId` INT, IN `p_ObjectHealth` VARCHAR(1024), IN `p_ObjectDamage` DOUBLE)
 	LANGUAGE SQL
 	NOT DETERMINISTIC
 	CONTAINS SQL
 	SQL SECURITY DEFINER
 	COMMENT ''
 BEGIN
-	UPDATE objects SET health=param_ObjectHealth,damage=param_ObjectDamage WHERE id=param_ObjectId;
+	UPDATE objects SET health=p_ObjectHealth,damage=p_ObjectDamage WHERE id=p_ObjectId; --
 END;
 
 
 DROP PROCEDURE IF EXISTS `updII`;
-CREATE PROCEDURE `proc_updateObjectInventory`(IN `param_ObjectId` int, IN `param_ObjectInventory` varchar(1024))
+CREATE PROCEDURE `proc_updateObjectInventory`(IN `p_ObjectId` int, IN `p_ObjectInventory` varchar(1024))
 	LANGUAGE SQL
 	NOT DETERMINISTIC
 	CONTAINS SQL
 	SQL SECURITY DEFINER
 	COMMENT ''
 BEGIN
-	UPDATE objects SET inventory=param_ObjectInventory WHERE id = param_ObjectId; 
+	UPDATE objects SET inventory=p_ObjectInventory WHERE id = p_ObjectId; --
 END;
 
 
 DROP PROCEDURE IF EXISTS `updIPF`;
-CREATE PROCEDURE `proc_updateObjectPosition`(IN `param_ObjectId` INT, IN `param_ObjectPosition` VARCHAR(255), IN `param_ObjectFuel` DOUBLE)
+CREATE PROCEDURE `proc_updateObjectPosition`(IN `p_ObjectId` INT, IN `p_ObjectPosition` VARCHAR(255), IN `p_ObjectFuel` DOUBLE)
 	LANGUAGE SQL
 	NOT DETERMINISTIC
 	CONTAINS SQL
 	SQL SECURITY DEFINER
 	COMMENT ''
 BEGIN
-	UPDATE objects SET pos=if(param_ObjectPosition='[]', pos, param_ObjectPosition),fuel=param_ObjectFuel WHERE id = param_ObjectId;
+	UPDATE objects SET pos=if(p_ObjectPosition='[]', pos, p_ObjectPosition),fuel=p_ObjectFuel WHERE id = p_ObjectId; --
 END;
 
 
 DROP PROCEDURE IF EXISTS `updUI`;
-CREATE PROCEDURE `proc_updateObjectInventoryByUID`(IN `param_ObjectUniqueId` varchar(50), IN `param_ObjectInventory` varchar(8192))
+CREATE PROCEDURE `proc_updateObjectInventoryByUID`(IN `p_ObjectUniqueId` varchar(50), IN `p_ObjectInventory` varchar(8192))
 	LANGUAGE SQL
 	NOT DETERMINISTIC
 	CONTAINS SQL
 	SQL SECURITY DEFINER
 	COMMENT ''
 BEGIN
-	UPDATE objects SET inventory = param_ObjectInventory WHERE uid NOT LIKE '%.%' AND (CONVERT(uid, UNSIGNED INTEGER) BETWEEN (CONVERT(param_ObjectUniqueId, UNSIGNED INTEGER) - 2) AND (CONVERT(param_ObjectUniqueId, UNSIGNED INTEGER) + 2));
+	UPDATE objects SET inventory = p_ObjectInventory WHERE uid NOT LIKE '%.%' AND (CONVERT(uid, UNSIGNED INTEGER) BETWEEN (CONVERT(p_ObjectUniqueId, UNSIGNED INTEGER) - 2) AND (CONVERT(p_ObjectUniqueId, UNSIGNED INTEGER) + 2)); --
 END;
 
 
 DROP PROCEDURE IF EXISTS `updV`;
-CREATE PROCEDURE `proc_updateObject`(IN `param_ObjectUniqueId` VARCHAR(50), IN `param_ObjectType` VARCHAR(255) , IN `param_ObjectPosition` VARCHAR(255), IN `param_ObjectHealth` VARCHAR(1024))
+CREATE PROCEDURE `proc_updateObject`(IN `p_ObjectUniqueId` VARCHAR(50), IN `p_ObjectType` VARCHAR(255) , IN `p_ObjectPosition` VARCHAR(255), IN `p_ObjectHealth` VARCHAR(1024))
 	LANGUAGE SQL
 	NOT DETERMINISTIC
 	CONTAINS SQL
 	SQL SECURITY DEFINER
 	COMMENT ''
 BEGIN
-	UPDATE objects SET otype=if(param_ObjectType='',otype,param_ObjectType),health=param_ObjectHealth,pos=if(param_ObjectPosition='[]',pos,param_ObjectPosition) WHERE uid=param_ObjectUniqueId;
+	UPDATE objects SET otype=if(p_ObjectType='',otype,p_ObjectType),health=p_ObjectHealth,pos=if(p_ObjectPosition='[]', pos, p_ObjectPosition) WHERE uid=p_ObjectUniqueId; --
 END;
 
 
