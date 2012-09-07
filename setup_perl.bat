d5e8eb5a0f2d522d832db5981f1c36c9db2e425f
ayan4m1 <andrew@bulletlogic.com>
2012-09-07 14:16:16 -0400

    fix #110 (resolve cpan stopping bat execution)

--- setup_perl.bat
+++ setup_perl.bat
@@ -1,5 +1,4 @@
 @echo off
-cpan File::Basename::Object
 cd util\DBIx-Transaction-1.100
 perl Makefile.PL
 dmake
@@ -9,3 +8,4 @@ perl Makefile.PL
 dmake
 dmake install
 cd ..\..
+cpan File::Basename::Object
