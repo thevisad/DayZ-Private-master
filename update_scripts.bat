78999ba64a5eacdd458dc3808a02f9cc4e9c2799
ayan4m1 <andrew@bulletlogic.com>
2012-09-09 22:10:59 -0400

    update_scripts.bat/sh now update BlissLingor files

--- update_scripts.bat
+++ update_scripts.bat
@@ -6,3 +6,11 @@ echo Updated scripts.txt
 echo Updated remoteexec.txt
 ..\..\..\util\wget.exe --quiet -N http://dayz-community-banlist.googlecode.com/git/filters/createvehicle.txt
 echo Updated createvehicle.txt
+
+cd ..\..\BlissLingor\BattlEye
+..\..\..\util\wget.exe --quiet -N http://dayz-community-banlist.googlecode.com/git/filters/scripts.txt
+echo Updated scripts.txt
+..\..\..\util\wget.exe --quiet -N http://dayz-community-banlist.googlecode.com/git/filters/remoteexec.txt
+echo Updated remoteexec.txt
+..\..\..\util\wget.exe --quiet -N http://dayz-community-banlist.googlecode.com/git/filters/createvehicle.txt
+echo Updated createvehicle.txt
--- update_scripts.sh
+++ update_scripts.sh
@@ -6,3 +6,11 @@ wget --quiet -N http://dayz-community-banlist.googlecode.com/git/filters/remotee
 echo "Updated remoteexec.txt"
 wget --quiet -N http://dayz-community-banlist.googlecode.com/git/filters/createvehicle.txt
 echo "Updated createvehicle.txt"
+
+cd ../../BlissLingor/BattlEye/
+wget --quiet -N http://dayz-community-banlist.googlecode.com/git/filters/scripts.txt
+echo "Updated Lingor scripts.txt"
+wget --quiet -N http://dayz-community-banlist.googlecode.com/git/filters/remoteexec.txt
+echo "Updated Lingor remoteexec.txt"
+wget --quiet -N http://dayz-community-banlist.googlecode.com/git/filters/createvehicle.txt
+echo "Updated Lingor createvehicle.txt"
