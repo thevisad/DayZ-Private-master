8ce5fe9a709796068d9a9a0a6841f98423700d55
ayan4m1 <andrew@bulletlogic.com>
2012-09-10 22:59:58 -0400

    add placeholders for repack.pl to replace

--- deploy/server.bat
+++ deploy/server.bat
@@ -1,1 +1,1 @@
-"Expansion\beta\arma2oaserver.exe" -port=2302 "-config=Bliss\config.cfg" "-cfg=Bliss\basic.cfg" "-profiles=Bliss" -name=Bliss "-mod=@DayZ;@Bliss;" -cpuCount=2 -maxMem=1578
+"Expansion\beta\arma2oaserver.exe" -port=2302 "-config=Bliss\config_deadbeef.cfg" "-cfg=Bliss\basic.cfg" "-profiles=Bliss" -name=Bliss "-mod=@DayZ;@Bliss;" -cpuCount=2 -maxMem=1578
