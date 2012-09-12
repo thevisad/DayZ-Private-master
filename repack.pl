f7142ab1ee1c1f0347a5a03d63df7970f619c2e0
ayan4m1 <andrew@bulletlogic.com>
2012-09-12 18:23:25 -0400

    close #135 (clean up and fix repack.pl)

--- repack.pl
+++ repack.pl
@@ -13,61 +13,70 @@ make_path(
 	'./deploy/MPMissions'
 );
 
-my $baseDir = "./bliss/missions";
+my $deployDir = "./deploy";
+my $missionDir = "./bliss/missions";
+my @profiles = ('Bliss', 'BlissLingor');
 
-# Set OS-specific options and copy blisshive.dll into place
-my $repackCmd, $copyCmd;
-my $editOpts = "";
+# Set OS-specific options
+my $repackCmd, $editOpts = '';
 if ($^O eq "linux") {
 	$repackCmd = "wine util/cpbo.exe";
-	system("cp ./util/blisshive.dll ./deploy/\@Bliss");
-	system("cp ./util/blisshive.dll ./deploy/\@BlissLingor");
 } elsif ($^O eq "MSWin32") {
 	$repackCmd = "util/cpbo.exe";
-	system("copy .\\util\\blisshive.dll .\\deploy\\\@Bliss");
-	system("copy .\\util\\blisshive.dll .\\deploy\\\@BlissLingor");
 	$editOpts = ".bak";
 }
 
-# If the profile directories have not been obfuscated
-my @profiles = ('./deploy/Bliss', './deploy/BlissLingor');
-foreach $dir (@profiles) {
-	if (-d $dir && -f "$dir/basic.cfg" && -f "$dir/config.cfg") {
+# Copy blisshive.dll into place
+foreach $profile (@profiles) {
+	$cmd = "cp ./util/blisshive.dll $deployDir/\@$profile";
+	if ($^O eq "MSWin32") {
+		$cmd =~ s/cp/copy/;
+		$cmd =~ s/\//\\/g;
+	}
+	system($cmd);
+}
+
+# Obfuscate unmodified profile directories
+foreach $profile (@profiles) {
+	my $dir = "$deployDir/$profile";	
+	if (-d $deployDir && -f "$dir/basic.cfg" && -f "$dir/config.cfg") {
+		print "INFO: Suffixing key $hash for $dir\n";
 		my $start = int(rand(32));
 		my $hash = substr(sha1_hex(time()), $start, 8);
-		rename("$dir/config.cfg", "${dir}/config_$hash.cfg");
-		if ($dir =~ /Lingor/) {
-			system("perl -pi${editOpts} -e 's/config=BlissLingor\\\\config_[0-9a-fA-F]{8}\\.cfg/config=BlissLingor\\\\config_${hash}.cfg/' ./deploy/server_lingor.bat\n");
-		} else {
-			system("perl -pi${editOpts} -e 's/config=Bliss\\\\config_[0-9a-fA-F]{8}\\.cfg/config=Bliss\\\\config_${hash}.cfg/' ./deploy/server.bat\n");
+		rename("$dir/config.cfg", "$dir/config_$hash.cfg");
+
+		my $batFile = ($dir =~ /Lingor/) ? "$deployDir/server_lingor.bat" : "$deployDir/server.bat";
+		system("perl -pi$editOpts -e \"s/config=$profile\\\\config_[0-9a-fA-F]{8}\\.cfg/config=$profile\\\\config_${hash}.cfg/\" $batFile");
+		# Clean up server.bat.bak in Windows only
+		if ($^O eq "MSWin32") {
+			(my $winPath = $batFile) =~ s/\//\\/g;
+			system("del \"$winPath.bak\"");
 		}
-		print "INFO: Suffixing key $hash for $dir\n";
 	}
 }
 
 # Pack the server files
-system("${repackCmd} -y -p ./bliss/dayz_server ./deploy/\@Bliss/addons/dayz_server.pbo");
-system("${repackCmd} -y -p ./bliss/dayz_lingor ./deploy/\@BlissLingor/addons/dayz_server.pbo");
+system("$repackCmd -y -p ./bliss/dayz_server $deployDir/\@Bliss/addons/dayz_server.pbo");
+system("$repackCmd -y -p ./bliss/dayz_lingor $deployDir/\@BlissLingor/addons/dayz_server.pbo");
 
 # Find our mission directories
-opendir(my $dh, $baseDir) or die ('Could not open ./bliss/missions!');
+opendir(my $dh, $missionDir) or die ("Could not open ${missionDir}!");
 @missions = grep { !/^\.\.?$/ } readdir($dh);
 closedir($dh);
 
 # Replace the instance ID dynamically
 foreach my $dir (@missions) {
-	my $path = "${baseDir}/${dir}";
+	my $path = "${missionDir}/${dir}";
 	if (-d $path) {	
 		if ($dir =~ /dayz_([0-9]{1,}).*/) {
-			my $id = $1;
-			system("perl -pi${editOpts} -e \"s/dayZ_instance\\s=\\s[0-9]*/dayZ_instance = ${id}/\" ${path}/init.sqf");
-			print "Set instance id to ${id} for ${path}/init.sqf\n";
+			system("perl -pi$editOpts -e \"s/dayZ_instance\\s=\\s[0-9]*/dayZ_instance = $1/\" $path/init.sqf");
+			print "Set instance id to $1 for $path/init.sqf\n";
 		}
 		# Clean up init.sqf.bak in Windows only
 		if ($^O eq "MSWin32") {
 			(my $winPath = $path) =~ s/\//\\/g;
-			system("del \"${winPath}\\init.sqf.bak\"");
+			system("del \"$winPath\\init.sqf.bak\"");
 		}
-		system("${repackCmd} -y -p ${path} ./deploy/MPMissions/${dir}.pbo");
+		system("$repackCmd -y -p $path $deployDir/MPMissions/$dir.pbo");
 	}
 }
