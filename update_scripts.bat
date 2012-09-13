57725bff37a2863d493154a7e6a770fd897be6a6
ayan4m1 <andrew@bulletlogic.com>
2012-09-13 08:10:26 -0400

    add publicvariable.txt to BE text files handled by update_scripts.bat/sh (thanks DzAirmaX!)

--- update_scripts.bat
+++ update_scripts.bat
@@ -1,16 +1,22 @@
 @echo off
 cd deploy\Bliss\BattlEye
-..\..\..\util\wget.exe --quiet -N http://dayz-community-banlist.googlecode.com/git/filters/scripts.txt
+echo Changed to Bliss directory
+..\..\..\util\wget.exe --quiet -N https://dayz-community-banlist.googlecode.com/git/filters/scripts.txt
 echo Updated scripts.txt
-..\..\..\util\wget.exe --quiet -N http://dayz-community-banlist.googlecode.com/git/filters/remoteexec.txt
+..\..\..\util\wget.exe --quiet -N https://dayz-community-banlist.googlecode.com/git/filters/remoteexec.txt
 echo Updated remoteexec.txt
-..\..\..\util\wget.exe --quiet -N http://dayz-community-banlist.googlecode.com/git/filters/createvehicle.txt
+..\..\..\util\wget.exe --quiet -N https://dayz-community-banlist.googlecode.com/git/filters/createvehicle.txt
 echo Updated createvehicle.txt
+..\..\..\util\wget.exe --quiet -N https://dayz-community-banlist.googlecode.com/git/filters/publicvariable.txt
+echo Updated publicvariable.txt
 
 cd ..\..\BlissLingor\BattlEye
-..\..\..\util\wget.exe --quiet -N http://dayz-community-banlist.googlecode.com/git/filters/scripts.txt
+echo Changed to BlissLingor directory
+..\..\..\util\wget.exe --quiet -N https://dayz-community-banlist.googlecode.com/git/filters/scripts.txt
 echo Updated scripts.txt
-..\..\..\util\wget.exe --quiet -N http://dayz-community-banlist.googlecode.com/git/filters/remoteexec.txt
+..\..\..\util\wget.exe --quiet -N https://dayz-community-banlist.googlecode.com/git/filters/remoteexec.txt
 echo Updated remoteexec.txt
-..\..\..\util\wget.exe --quiet -N http://dayz-community-banlist.googlecode.com/git/filters/createvehicle.txt
+..\..\..\util\wget.exe --quiet -N https://dayz-community-banlist.googlecode.com/git/filters/createvehicle.txt
 echo Updated createvehicle.txt
+..\..\..\util\wget.exe --quiet -N https://dayz-community-banlist.googlecode.com/git/filters/publicvariable.txt
+echo Updated publicvariable.txt
--- update_scripts.sh
+++ update_scripts.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 cd deploy/Bliss/BattlEye/
-wget --quiet -N http://dayz-community-banlist.googlecode.com/git/filters/scripts.txt
+echo "Changed directory to Bliss"
+wget --quiet -N https://dayz-community-banlist.googlecode.com/git/filters/scripts.txt
 echo "Updated scripts.txt"
-wget --quiet -N http://dayz-community-banlist.googlecode.com/git/filters/remoteexec.txt
+wget --quiet -N https://dayz-community-banlist.googlecode.com/git/filters/remoteexec.txt
 echo "Updated remoteexec.txt"
-wget --quiet -N http://dayz-community-banlist.googlecode.com/git/filters/createvehicle.txt
+wget --quiet -N https://dayz-community-banlist.googlecode.com/git/filters/createvehicle.txt
 echo "Updated createvehicle.txt"
+wget --quiet -N https://dayz-community-banlist.googlecode.com/git/filters/publicvariable.txt
+echo "Updated publicvariable.txt"
 
 cd ../../BlissLingor/BattlEye/
-wget --quiet -N http://dayz-community-banlist.googlecode.com/git/filters/scripts.txt
-echo "Updated Lingor scripts.txt"
-wget --quiet -N http://dayz-community-banlist.googlecode.com/git/filters/remoteexec.txt
-echo "Updated Lingor remoteexec.txt"
-wget --quiet -N http://dayz-community-banlist.googlecode.com/git/filters/createvehicle.txt
-echo "Updated Lingor createvehicle.txt"
+echo "Changed directory to BlissLingor"
+wget --quiet -N https://dayz-community-banlist.googlecode.com/git/filters/scripts.txt
+echo "Updated scripts.txt"
+wget --quiet -N https://dayz-community-banlist.googlecode.com/git/filters/remoteexec.txt
+echo "Updated remoteexec.txt"
+wget --quiet -N https://dayz-community-banlist.googlecode.com/git/filters/createvehicle.txt
+echo "Updated createvehicle.txt"
+wget --quiet -N https://dayz-community-banlist.googlecode.com/git/filters/publicvariable.txt
+echo "Updated publicvariable.txt"
