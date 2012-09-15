67fcdc482df8221c180eae467835c1f8ce272811
ayan4m1 <andrew@bulletlogic.com>
2012-09-15 16:40:41 -0400

    close #142 (repack.pl randomizes default admin passwords)

--- repack.pl
+++ repack.pl
@@ -18,12 +18,9 @@ my $missionDir = "./bliss/missions";
 my @profiles = ('Bliss', 'BlissLingor');
 
 # Set OS-specific options
-my $repackCmd, $editOpts = '';
-if ($^O eq "linux") {
-	$repackCmd = "wine util/cpbo.exe";
-} elsif ($^O eq "MSWin32") {
-	$repackCmd = "util/cpbo.exe";
-	$editOpts = ".bak";
+my $repackCmd = "wine util/cpbo.exe";
+if ($^O eq "MSWin32") {
+	$repackCmd =~ s/wine\s//;
 }
 
 # Copy blisshive.dll into place
@@ -40,18 +37,20 @@ foreach $profile (@profiles) {
 foreach $profile (@profiles) {
 	my $dir = "$deployDir/$profile";	
 	if (-d $deployDir && -f "$dir/basic.cfg" && -f "$dir/config.cfg") {
-		print "INFO: Suffixing key $hash for $dir\n";
 		my $start = int(rand(32));
 		my $hash = substr(sha1_hex(time()), $start, 8);
-		rename("$dir/config.cfg", "$dir/config_$hash.cfg");
+		print "INFO: Suffixing key $hash for $dir\n";
 
-		my $batFile = ($dir =~ /Lingor/) ? "$deployDir/server_lingor.bat" : "$deployDir/server.bat";
-		system("perl -pi$editOpts -e \"s/config=$profile\\\\config_[0-9a-fA-F]{8}\\.cfg/config=$profile\\\\config_${hash}.cfg/\" $batFile");
-		# Clean up server.bat.bak in Windows only
-		if ($^O eq "MSWin32") {
-			(my $winPath = $batFile) =~ s/\//\\/g;
-			system("del \"$winPath.bak\"");
-		}
+		# Insert hash as default admin password
+		replaceText("s/passwordAdmin\\s=\\s\\\"\\\"/passwordAdmin = \\\"$hash\\\"/", "$dir/config.cfg");
+		replaceText("s/RConPassword\\sCHANGEME/RConPassword $hash/", "$dir/BattlEye/BEServer.cfg");
+
+		# Rename config file and update related .bat file
+		rename("$dir/config.cfg", "$dir/config_$hash.cfg");
+		replaceText(
+			"s/config=$profile\\\\config_[0-9a-fA-F]{8}\\.cfg/config=$profile\\\\config_${hash}.cfg/",
+			(($dir =~ /Lingor/) ? "$deployDir/server_lingor.bat" : "$deployDir/server.bat")
+		);
 	}
 }
 
@@ -67,16 +66,23 @@ closedir($dh);
 # Replace the instance ID dynamically
 foreach my $dir (@missions) {
 	my $path = "${missionDir}/${dir}";
-	if (-d $path) {	
+	if (-d $path) {
+		# If directory has valid instance ID, replace it in init.sqf	
 		if ($dir =~ /dayz_([0-9]{1,}).*/) {
-			system("perl -pi$editOpts -e \"s/dayZ_instance\\s=\\s[0-9]*/dayZ_instance = $1/\" $path/init.sqf");
+			replaceText("s/dayZ_instance\\s=\\s[0-9]*/dayZ_instance = $1/", "$path/init.sqf");
 			print "Set instance id to $1 for $path/init.sqf\n";
 		}
-		# Clean up init.sqf.bak in Windows only
-		if ($^O eq "MSWin32") {
-			(my $winPath = $path) =~ s/\//\\/g;
-			system("del \"$winPath\\init.sqf.bak\"");
-		}
+		# Pack the mission files
 		system("$repackCmd -y -p $path $deployDir/MPMissions/$dir.pbo");
+	}
+}
+
+# Cross-platform replacement of text using system()
+sub replaceText {
+	system("perl -pi" . (($^O eq "MSWin32") ? '.bak' : '') . " -e \"$_[0]\" $_[1]");
+	# Clean up .bak file in Windows only
+	if ($^O eq "MSWin32") {
+		(my $winPath = $_[1]) =~ s/\//\\/g;
+		system("del \"$winPath.bak\"");
 	}
 }
