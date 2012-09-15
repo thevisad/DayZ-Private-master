40052493c14b041a019e5c3ce7017b28fd854310
ayan4m1 <andrew@bulletlogic.com>
2012-09-15 16:45:35 -0400

    fix #140, fix #138 (added schema 0.16 to fix helicopter spawn, incorrect log_code value [thanks Azrael!])

--- db_migrate.pl
+++ db_migrate.pl
@@ -30,7 +30,7 @@ if ($args{'help'}) {
 	exit;
 }
 
-my $version = $args{'version'} ? $args{'version'} : "0.15";
+my $version = $args{'version'} ? $args{'version'} : "0.16";
 
 print "INFO: Trying to connect to $db{'host'}, database $db{'name'} as $db{'user'}\n";
 
---
+++ schema/Bliss/mysql/0.15-0.16/10_heli_fix.sql
@@ -0,0 +1,1 @@
+update spawns set pos='[58,[10571.7,2220.31,0.0015564]]' where uuid = 38;
