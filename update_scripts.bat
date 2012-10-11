6f4186d3495c67dacabb352941ef1dff222fd56d
ayan4m1 <andrew@bulletlogic.com>
2012-10-11 10:05:17 -0400

    re-add update_scripts.bat/sh, update BE text files with latest changes from official

---
+++ update_scripts.bat
@@ -0,0 +1,13 @@
+@echo off
+
+cd util\dayz_config\BattlEye\
+echo "Updating util\dayz_config\BattlEye"
+wget --quiet -N https://dayz-community-banlist.googlecode.com/git/filters/scripts.txt
+wget --quiet -N https://dayz-community-banlist.googlecode.com/git/filters/remoteexec.txt
+wget --quiet -N https://dayz-community-banlist.googlecode.com/git/filters/createvehicle.txt
+wget --quiet -N https://dayz-community-banlist.googlecode.com/git/filters/publicvariable.txt
+wget --quiet -N https://dayz-community-banlist.googlecode.com/git/filters/publicvariableval.txt
+wget --quiet -N https://dayz-community-banlist.googlecode.com/git/filters/publicvariablevar.txt
+wget --quiet -N https://dayz-community-banlist.googlecode.com/git/filters/setpos.txt
+wget --quiet -N https://dayz-community-banlist.googlecode.com/git/filters/mpeventhandler.txt
+wget --quiet -N https://dayz-community-banlist.googlecode.com/git/filters/setdamage.txt
