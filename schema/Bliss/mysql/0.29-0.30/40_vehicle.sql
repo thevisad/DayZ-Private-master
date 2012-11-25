eb35aedcc9b21d3f6e7e2bd7f417fbb6e8c54cb7
ayan4m1 <andrew@bulletlogic.com>
2012-11-25 17:26:14 -0500

    close #496 (added 1.7.4 vehicle spawns [thanks R4Z0R49!])

---
+++ schema/Bliss/mysql/0.29-0.30/40_vehicle.sql
@@ -0,0 +1,6 @@
+insert into vehicle (id, class_name, damage_min, damage_max, fuel_min, fuel_max, limit_min, limit_max) values
+  (53, 'AH6X_DZ', 0.2, 0.8, 0.5, 0.7, 0, 1),
+  (54, 'Mi17_DZ', 0.3, 0.9, 0.5, 0.7, 0, 1),
+  (55, 'AN2_DZ', 0.6, 0.9, 0.2, 0.5, 0, 1),
+  (56, 'BAF_Offroad_D', 0.0, 0.8, 0.3, 0.6, 0, 1),
+  (57, 'BAF_Offroad_W', 0.0, 0.8, 0.3, 0.6, 0, 1);
