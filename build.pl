a5cf28ad53b795865ec4d9715b55a73828b5e309
thevisad <thevisad@gmail.com>
2013-02-05 01:05:48 -0500

    Correction to regex.

--- build.pl
+++ build.pl
@@ -173,7 +173,7 @@ if (-d "$wld_dir/$args{'world'}") {
 my @pkgs = ();
 my @msn_pkgs = ();
 while (my $option = shift(@ARGV)) {
-	next unless ($option =~ m/with-([.-\w]+)/);
+	next unless ($option =~ m/with-([-\w]+)/);
 
 	my $pkg_dir = "$base_dir/pkg/$1";
 	if (!-d $pkg_dir) {
