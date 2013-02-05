thevisad/DayZ-Private-master

commit 8ac997fd1b3bd4ee13a0cae6110b7e6907f3cd4c

Clean up of additional code, added in new sql file to correct missing instances in instance.

db_spawn_vehicles.pl
@@ -48,7 +48,7 @@ my $dbh = DBI->connect(
 	$db{'pass'}
 ) or die "FATAL: Could not connect to MySQL -  " . DBI->errstr . "\n";
 
-my ($world_id, $world_name) = $dbh->selectrow_array("select w.id, w.name from instance i join world w on i.world_id = w.id where i.id = ?", undef, ($db{'instance'}));
+my ($world_id, $world_name) = $dbh->selectrow_array("select w.id, w.name from instance i join world w on i.world_id = w.id where i.world_id = ?", undef, ($db{'instance'}));
 die "FATAL: Invalid instance ID\n" unless (defined $world_id && defined $world_name);
 
 print "INFO: Instance name dayz_$db{'instance'}.$world_name\n";

build_AllWorlds.cmd
@@ -1,25 +1,14 @@
 perl build.pl --clean
 perl build.pl --world chernarus --with-killmsgs --with-wrecks --with-invcust --with-carepkgs --instance 1
- pause
-perl build.pl --world fallujah --with-killmsgs --with-wrecks --with-invcust --with-carepkgs --instance 1
- pause
-perl build.pl --world lingor --with-killmsgs --with-wrecks --with-invcust --with-carepkgs --instance 1
- pause
-perl build.pl --world mbg_celle2 --with-killmsgs --with-wrecks --with-invcust --with-carepkgs --instance 1
- pause
-perl build.pl --world namalsk --with-wrecks --with-invcust --with-carepkgs --instance 1
- pause
-perl build.pl --world panthera2 --with-killmsgs --with-wrecks --with-invcust --with-carepkgs --instance 1
- pause
-perl build.pl --world takistan --with-killmsgs --with-wrecks --with-invcust --with-carepkgs --instance 1
- pause
-perl build.pl --world tavi --with-killmsgs --with-wrecks --with-invcust --with-carepkgs --with-tavi-com --instance 1 
- pause
-perl build.pl --world thirsk --with-invcust --instance 1
- pause
-perl build.pl --world thirskw --with-invcust --instance 2
- pause
-perl build.pl --world utes --with-killmsgs --with-wrecks --with-invcust --with-carepkgs --instance 1
- pause
-perl build.pl --world zargabad --with-killmsgs --with-wrecks --with-invcust --with-carepkgs --instance 1
- pause
+perl build.pl --world fallujah --with-killmsgs --with-wrecks --with-invcust --with-carepkgs --instance 6
+perl build.pl --world lingor --with-killmsgs --with-wrecks --with-invcust --with-carepkgs --instance 2
+perl build.pl --world mbg_celle2 --with-killmsgs --with-wrecks --with-invcust --with-carepkgs --instance 9
+perl build.pl --world namalsk --with-wrecks --with-invcust --with-carepkgs --instance 8
+perl build.pl --world panthera2 --with-killmsgs --with-wrecks --with-invcust --with-carepkgs --instance 5
+perl build.pl --world takistan --with-killmsgs --with-wrecks --with-invcust --with-carepkgs --instance 4
+perl build.pl --world tavi --with-wrecks --with-invcust --with-carepkgs --with-tavi-com --instance 10
+perl build.pl --world thirsk --with-invcust --instance 11
+perl build.pl --world thirskw --with-invcust --instance 13
+perl build.pl --world utes --with-killmsgs --with-wrecks --with-invcust --with-carepkgs --instance 3
+perl build.pl --world zargabad --with-killmsgs --with-wrecks --with-invcust --with-carepkgs --instance 7
+pause

db_migrate.pl
@@ -41,7 +41,7 @@ if ($args{'help'}) {
 die "FATAL: Schema version must be specified for non-standard schema!\n" if ($args{'schema'} && !defined $args{'version'});
 
 my $schema  = $args{'schema'} ? $args{'schema'} : "Reality";
-my $version = $args{'version'} ? $args{'version'} : "0.36";
+my $version = $args{'version'} ? $args{'version'} : "0.37";
 
 print "INFO: Trying to connect to $db{'host'}, database $db{'name'} as $db{'user'}\n";
 

db_migrate_worlds.cmd
@@ -1,0 +1,18 @@
+db_migrate.pl --host 127.0.0.1 --user redbone --pass hound --name dayz --port 3306 --version 0.36
+
+db_migrate.pl --host 127.0.0.1 --user redbone --pass hound --name dayz --port 3306 --schema RealityBuildings --version 0.01
+
+db_migrate.pl --host 127.0.0.1 --user redbone --pass hound --name dayz --port 3306 --schema RealityMessaging  -version 0.01
+
+db_migrate.pl --host 127.0.0.1 --user redbone --pass hound --name dayz --port 3306 --schema RealityInvCust  -version 0.02
+
+db_migrate.pl --host 127.0.0.1 --user redbone --pass hound --name dayz --port 3306 --schema RealityNamalsk --version 0.01
+
+db_migrate.pl --host 127.0.0.1 --user redbone --pass hound --name dayz --port 3306 --schema RealityTaviCom --version 0.01 
+
+db_migrate.pl --host 127.0.0.1 --user redbone --pass hound --name dayz --port 3306 --schema RealityThirsk --version 0.01 
+
+db_migrate.pl --host 127.0.0.1 --user redbone --pass hound --name dayz --port 3306 --schema RealityThirskWinter --version 0.01 
+
+db_migrate.pl --host 127.0.0.1 --user redbone --pass hound --name dayz --port 3306 --version 0.37
+pause

mission/tavi-com/init.sqf
@@ -6,7 +6,7 @@ cutText ["","BLACK OUT"];
 enableSaving [false, false];
 
 //REALLY IMPORTANT VALUES
-dayZ_instance =	1337;					//The instance
+dayZ_instance =	1;					//The instance
 dayzHiveRequest = [];
 initialized = false;
 dayz_previousID = 0;
@@ -14,26 +14,20 @@ dayz_previousID = 0;
 //disable greeting menu 
 player setVariable ["BIS_noCoreConversations", true];
 //disable radio messages to be heard and shown in the left lower corner of the screen
-enableRadio false;
 
 //Load in compiled functions
 call compile preprocessFileLineNumbers "\z\addons\dayz_code\init\variables.sqf";				//Initilize the Variables (IMPORTANT: Must happen very early)
-
 // (Taviana) Override some of the variables:
 call compile preprocessFileLineNumbers "\kh\dayztaviana\init\variables.sqf";
-
 progressLoadingScreen 0.1;
-call compile preprocessFileLineNumbers "\z\addons\dayz_code\init\publicEH.sqf";	//Initilize the publicVariable event handlers
+call compile preprocessFileLineNumbers "\z\addons\dayz_code\init\publicEH.sqf";				//Initilize the publicVariable event handlers
 progressLoadingScreen 0.2;
-call compile preprocessFileLineNumbers "\z\addons\dayz_code\medical\setup_functions_med.sqf"; //Functions used by CLIENT for medical
+call compile preprocessFileLineNumbers "\z\addons\dayz_code\medical\setup_functions_med.sqf";	//Functions used by CLIENT for medical
 progressLoadingScreen 0.4;
-
 // (Taviana) Don't call the regular compile:
 // call compile preprocessFileLineNumbers "\z\addons\dayz_code\init\compiles.sqf"; //Compile regular functions
-
 // (Taviana) Instead, call a copy of the DayZ 1.7.4.4 compiles:
 call compile preprocessFileLineNumbers "\kh\dayztaviana\init\compiles.sqf"; //Compile regular functions
-
 progressLoadingScreen 1.0;
 
 "filmic" setToneMappingParams [0.153, 0.357, 0.231, 0.1573, 0.011, 3.750, 6, 4]; setToneMapping "Filmic";

mission/tavi-com/onpause.sqf
@@ -1,34 +1,0 @@
-private["_display","_btnRespawn","_btnAbort","_timeOut","_timeMax"];
-disableSerialization;
-_display = _this select 0;
-_btnRespawn = _display displayCtrl 1010;
-_btnAbort = _display displayCtrl 104;
-_btnRespawn ctrlEnable false;
-_btnAbort ctrlEnable false;
-_timeOut = 0;
-_timeMax = 30;
-
-while {!isNull _display} do {
-	switch true do {
-		case ({isPlayer _x} count (player nearEntities ["AllVehicles", 6]) > 1) : {
-			_btnAbort ctrlEnable false;
-			cutText ["Cannot Abort near another player!", "PLAIN DOWN"];
-		};
-		case (_timeOut < _timeMax && count (player nearEntities ["zZombie_Base", 50]) > 0) : {
-			_btnAbort ctrlEnable false;
-			cutText [format ["Can Abort in %1", (_timeMax - _timeOut)], "PLAIN DOWN"];
-			_timeOut = _timeOut + 1;
-
-		};
-		case (player getVariable["combattimeout", 0] >= time) : {
-			_btnAbort ctrlEnable false;
-			cutText ["Cannot Abort while in combat!", "PLAIN DOWN"];
-		};
-		default {
-			_btnAbort ctrlEnable true;
-			cutText ["", "PLAIN DOWN"];
-		};
-	};
-	sleep 1;
-};
-cutText ["", "PLAIN DOWN"];

schema/Reality/mysql/0.35-0.36/41_world_vehicle.sql
@@ -1,5 +1,5 @@
-insert into world_vehicle values
- (873,92,1,'[122,[10137.868,12049.376,-6.1035156e-005]]',null,0.25,'0.36'),
- (874,93,1,'[0,[13441.056,12003.912,4.5776367e-005]]',null,0.4,'0.36');
+insert into world_vehicle ( vehicle_id, world_id, worldspace, description, chance, last_modified) values
+ (92,1,'[122,[10137.868,12049.376,-6.1035156e-005]]',null,0.25,'0.36'),
+ (93,1,'[0,[13441.056,12003.912,4.5776367e-005]]',null,0.4,'0.36');
 
 delete from world_vehicle where id = 300;

schema/Reality/mysql/0.36-0.37/10_instance.sql
@@ -1,0 +1,10 @@
+INSERT INTO instance (world_id, inventory, backpack )
+SELECT world.id, 
+instance.inventory,
+instance.backpack
+FROM 
+instance ,
+world
+WHERE world.id NOT IN (
+SELECT instance.id
+FROM instance);

commit 57451c8bce237bacb3f6c19784d0ed0d29e2893e

Update db_spawn_vehicles.pl
Commit 8ac997fd broke db_spawn_vehicles.pl for multiple instances.  Changing the SQL statement to search based on the instance ID instead of the world ID.

db_spawn_vehicles.pl
@@ -48,7 +48,7 @@ my $dbh = DBI->connect(
 	$db{'pass'}
 ) or die "FATAL: Could not connect to MySQL -  " . DBI->errstr . "\n";
 
-my ($world_id, $world_name) = $dbh->selectrow_array("select w.id, w.name from instance i join world w on i.world_id = w.id where i.world_id = ?", undef, ($db{'instance'}));
+my ($world_id, $world_name) = $dbh->selectrow_array("select w.id, w.name from instance i join world w on i.world_id = w.id where i.id = ?", undef, ($db{'instance'}));
 die "FATAL: Invalid instance ID\n" unless (defined $world_id && defined $world_name);
 
 print "INFO: Instance name dayz_$db{'instance'}.$world_name\n";