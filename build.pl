4c3eee0c7af5d5d17724faa12d9757aba1d37e0b
thevisad <thevisad@gmail.com>
2013-02-13 01:23:00 -0500

    Minor script corrections, corrected celle, updated control panel

--- build.pl
+++ build.pl
@@ -117,7 +117,7 @@ if (-d $src && !-d $conf_dir) {
 		'zargabad'    => '@dayzzargabad',
 		'panthera2'   => '@dayzpanthera',
 		'namalsk'     => '@dayz;@dayz_namalsk',
-		'mbg_celle2'  => '@dayz_celle;@mbg_celle',
+		'mbg_celle2'  => '@Dayz_Conflicts;@dayz_celle;@mbg_celle',
 		'tavi'        => '@taviana'
 	};
 	my $mod = ((defined $mods->{$args{'world'}}) ? "$mods->{$args{'world'}}" : '@dayz') . ";\@reality_$args{'instance'}.$args{'world'}";
