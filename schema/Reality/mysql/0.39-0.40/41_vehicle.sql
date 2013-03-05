2ad58fae8f08f3170a289b1934bb2f6df0c12331
Skaronator <DayZ@Skaronator.com>
2013-03-05 10:45:15 +0100

    Add Lingor Vehicles for 2.2

---
+++ schema/Reality/mysql/0.39-0.40/41_vehicle.sql
@@ -0,0 +1,13 @@
+insert ignore INTO `vehicle` (`id`, `class_name`, `limit_min`, `limit_max`, `parts`) VALUES
+(95, 'Ka137_DZ', 0, 1, NULL),
+(96, 'ibr_as350lingor', 0, 1, NULL),
+(97, 'ibr_as350_pmc', 0, 1, NULL),
+(98, 'ibr_as350_pol', 0, 1, NULL),
+(99, 'JetSkiYanahui_Case_Yellow', 0, 1, NULL),
+(100, 'JetSkiYanahui_Case_Green', 0, 1, NULL),
+(101, 'JetSkiYanahui_Case_White', 0, 1, NULL),
+(102, 'JetSkiYanahui_Case_Red', 0, 1, NULL),
+(103, 'JetSkiYanahui_Yellow', 0, 1, NULL),
+(104, 'JetSkiYanahui_Green', 0, 1, NULL),
+(105, 'JetSkiYanahui_White', 0, 1, NULL),
+(106, 'JetSkiYanahui_Red', 0, 1, NULL);
