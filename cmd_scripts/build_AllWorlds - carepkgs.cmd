72cc1f2f29609089a4ad68704d29155517af42b2
CraftLourens <megocrafter@scalpnet.ru>
2013-07-27 21:49:54 +0700

    Little fix

--- cmd_scripts/build_AllWorlds - carepkgs.cmd
+++ cmd_scripts/build_AllWorlds - carepkgs.cmd
@@ -1,11 +1,11 @@
-perl build.pl --clean
-perl build.pl --world chernarus --with-carepkgs 
+perl ..\build.pl --clean
+perl ..\build.pl --world chernarus --with-carepkgs 
 
-perl build.pl --world skaro.lingor --with-carepkgs
+perl ..\build.pl --world skaro.lingor --with-carepkgs
 
-perl build.pl --world thirsk --with-carepkgs 
+perl ..\build.pl --world thirsk --with-carepkgs 
 
-perl build.pl --world thirskw --with-carepkgs 
+perl ..\build.pl --world thirskw --with-carepkgs 
 
-perl build.pl --world utes --with-carepkgs 
+perl ..\build.pl --world utes --with-carepkgs 
  pause
