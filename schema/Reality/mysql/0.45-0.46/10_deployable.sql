493794adeeff94b21b2a70cfce4fcda7c4f1fced
thevisad <thevisad@gmail.com>
2013-09-18 09:10:18 -0400

    Updates scripts downloader to grab the latest version (1.8) Added new deployables to the database tables.
    Signed-off-by: thevisad <thevisad@gmail.com>

---
+++ schema/Reality/mysql/0.45-0.46/10_deployable.sql
@@ -0,0 +1,8 @@
+insert into deployable (class_name) values ('DomeTentStorage');
+insert into deployable (class_name) values ('CamoNet_DZ');
+insert into deployable (class_name) values ('Trap_Cans');
+insert into deployable (class_name) values ('TrapTripwireFlare');
+insert into deployable (class_name) values ('TrapBearTrapSmoke');
+insert into deployable (class_name) values ('TrapTripwireGrenade');
+insert into deployable (class_name) values ('TrapTripwireSmoke');
+insert into deployable (class_name) values ('TrapBearTrapFlare');
